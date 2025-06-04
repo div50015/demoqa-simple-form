dae761efeae3a97a0ff3a977a80409d50a948df2
Igor Degtyarenko <div50015@mail.ru>
2025-06-04 10:15:56 +0300

    completing the test editing

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 @pytest.fixture(scope='function',autouse=True)
 def browser_manager():
-    # browser.config.base_url='https://demoqa.com'
+    browser.config.base_url='https://demoqa.com'
     browser.config.timeout = 2.0
     browser.config.window_width = 900
     browser.config.window_height = 900
--- tests/demoqa_simple_form_registration.py
+++ tests/demoqa_simple_form_registration.py
@@ -1,19 +1,27 @@
-from selene import browser
+from selene import browser, have, be
 import pytest
 from time import sleep
 
+
 def test_registration_simple_form():
-    #GIVEN
-    browser.open('https://demoqa.com/elements')
+    # GIVEN
+    browser.open('/')
+    browser.element('//*[text() = "Elements"]').element('../..').click()
     browser.element('//*[text() = "Text Box"]').click()
+
+    # WHENtext-box
     browser.element('#userName').type('Igor Deg')
+    browser.element('#userEmail').type('div@novoch.ru')
+    browser.element('#currentAddress').type('Russia Novocherkassk')
+    browser.element('#permanentAddress').type('Russia Sochi')
+    browser.element('#submit').click()
 
-    pass
-
-    # sleep(5)
-    #WHENtext-box
-
-
-    #THEN
-
-
+    # THEN
+    browser.element('#output').all('p').should(
+        have.texts(
+            'Name:Igor Deg',
+            'Email:div@novoch.ru',
+            'Current Address :Russia Novocherkassk',
+            'Permananet Address :Russia Sochi',
+        )
+    )
